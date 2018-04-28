63e0a91a5c716cdc5846531f440a07a8c74d68af
Roger B <myvarequals@gmail.com>
2018-04-28 10:34:18 -0400

    add some js

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $(window).scroll(function(){
 
     var scrolling = $(this).scrollTop()
-
+    console.log(scrolling)
     $('.cliff-front-left, .cliff-front-right').css({
      'transform' : 'translate(0px, '+ scrolling * 0.5 +'px)'
     });
@@ -11,14 +11,35 @@ $(document).ready(function(){
      'transform' : 'translate(0px, '+ scrolling * 0.65 +'px)'
     });
 
-    if(scrolling > $('.gallery-box').offset().top) {
-      $('.gallery-box .gallery-item').each(function(){
+    if(scrolling > $('.skills-list').offset().top - 500) {
+      $('.skills-list .list').each(function(i){
 
-        $('.gallery-box .gallery-item').addClass('appear');
+        setTimeout(function(){
+          $('.skills-list .list').eq(i).addClass('skill-appear');
+        }, 150 * (i+1));
+      })
+    }
+
+
+
+  });
+
+  $(window).scroll(function(){
+    var scrolling = $(this).scrollTop();
+
+    if(scrolling > $('.gallery-box').offset().top - ($(window).height() / 1.6)) {
+      console.log($('.gallery-box').offset().top);
+      $('.gallery-box .gallery-item:nth-child(-n+3)').each(function(){
+
+        $('.gallery-box .gallery-item:nth-child(-n+3)').addClass('appear-top');
+
+      });
+      $('.gallery-box .gallery-item:nth-child(n+4)').each(function(){
+
+        $('.gallery-box .gallery-item:nth-child(n+4)').addClass('appear-bottom');
 
       });
     }
-
   });
 
 });

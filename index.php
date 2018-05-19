d2a0def2f1cfb154f6da39db6a1666db8a3132fb
Roger B <myvarequals@gmail.com>
2018-05-18 23:51:31 -0400

    work on contact form, added footer

--- index.php
+++ index.php
@@ -220,6 +220,11 @@
         <button type="submit" name ="submit">Submit</button>
       </form>
     </section>
+    <section id='footer-section'>
+      <div class='footer'>
+        <h3>Night Owl Dev 2018 &copy;</h3>
+      </div>
+    </section>
   </main>
   <script src="http://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
